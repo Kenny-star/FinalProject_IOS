5247d4fa08545948146031bc2e28c209cb6d2acb
Kenny <54686876+Kenny-star@users.noreply.github.com>
2021-11-30 00:12:12 -0500

    Add project files.

---
+++ FinalProject_IOS/FinalProject_IOS/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using FinalProject_IOS.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FinalProject_IOS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FinalProject_IOS/FinalProject_IOS/Views/LoginPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FinalProject_IOS.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Register_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
