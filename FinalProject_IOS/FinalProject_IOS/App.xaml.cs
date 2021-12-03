3285ff1010228698b3c83b05c7c46545cd4a3970
Ludovic Marcoux Hoyos <ludomarcoux098@gmail.com>
2021-12-02 20:25:42 -0500

    Added AccessSession Page

--- FinalProject_IOS/FinalProject_IOS/App.xaml.cs
+++ FinalProject_IOS/FinalProject_IOS/App.xaml.cs
@@ -11,7 +11,7 @@ namespace FinalProject_IOS
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new TeacherDashboard());
+            MainPage = new NavigationPage(new AccessSession());
         }
 
         protected override void OnStart()
