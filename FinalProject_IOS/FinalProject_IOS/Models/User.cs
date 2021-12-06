c0058a0464548154564319c1df3184e994090154
Kenny <54686876+Kenny-star@users.noreply.github.com>
2021-12-06 18:03:44 -0500

    Model Classes

---
+++ FinalProject_IOS/FinalProject_IOS/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject_IOS.Models
+{
+    class User
+    {
+        public string accountId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string courseId { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+        public Status userStatus { get; set; }
+    }
+
+    public enum Status
+    {
+        Pending,
+        Accepted,
+        Declined
+    }
+}
